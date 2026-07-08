533e0e432a46f2d69f08395a56d0f89f3d67a23a
Emilio <eamil1997@gmail.com>
2026-07-08 21:28:36 +0300

    chore: update contact email to emilabdumalikov@gmail.com

--- src/content/social.ts
+++ src/content/social.ts
@@ -5,7 +5,7 @@ export type SocialLink = {
 };
 
 export const socialLinks: SocialLink[] = [
-  { id: "email", href: "mailto:eamil1997@gmail.com", icon: "mail" },
+  { id: "email", href: "mailto:emilabdumalikov@gmail.com", icon: "mail" },
   { id: "github", href: "https://github.com/emilioaab", icon: "github" },
   { id: "linkedin", href: "https://www.linkedin.com/in/emilabdumalikov/", icon: "linkedin" },
 ];
